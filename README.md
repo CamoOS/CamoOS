6f2b33ab56c270eeeed12c24b3704caa2d2d4c30
0xC4aE1e5 <111705094+0xC4aE1e5@users.noreply.github.com>
2022-12-18 22:06:33 +0000

    2022-12-18 Rollup

--- README.md
+++ README.md
@@ -6,16 +6,23 @@ CamoOS is a free, private, and secure operating system built upon Windows LTSC.
 
 ## 🟥 What CamoOS is not
 
- - An ISO to improve performance for gaming, such as FoxOS
- - A program that helps with privacy, such as O&O ShutUp
- - A privacy-first ISO, such as AME. CamoOS is designed for security as well.
+- An ISO to improve performance for gaming, such as FoxOS
+- A program that helps with privacy, such as O&O ShutUp
+- A privacy-first ISO, such as AME. CamoOS is designed for security as well.
+
 ## 🟪 What's not supported in CamoOS?
 
-Almost everything that O&O ShutUp's not recommended level blocks/removes, which can easily be restored as needed.
+Everything that O&O ShutUp's not recommended level blocks/removes (except for some security-related options), which can easily be restored as needed.
 
-## 🟦 How do I compile CamoOS?
+## 🟦 How do I create a CamoOS ISO?
 
-Firstly, extract an LTSC 2021+ ISO using 7-Zip or similar tools (IoT and 32-bit not recommended and untested) (older versions may work, but untested), to a folder called DVD next to the build script. Then right click, build.cmd, and run as admin. It will perform it's necessary checks, and will produce an ISO.
+1. Firstly, copy an LTSC 2021+ ISO **(IoT will not work, 32-bit not recommended and untested, the former will have no support, older versions may work, but untested and will have no support)**, to a file called DVD.iso next to the build script.
+1. You will also need to make sure that your execution policy is Bypass or Unrestricted.
+1. Open a PowerShell window as admin.
+1. Hold Shift and right click build.ps1. Then click Copy as path.
+1. Then type `. <paste path>`, where with `<paste path>` you right click PowerShell to paste
+1. Press Enter to run.
+1. It will perform it's necessary checks, and will produce an ISO along with a SHA1 checksum.
 
 ## 🟨 How do I install CamoOS?
 
@@ -24,3 +31,27 @@ It is as similar as installing regular Windows.
 ## 🟥 How do I activate CamoOS?
 
 CamoOS comes activated out of the box, with MAS.
+
+## 🟦 FAQ
+
+<details>
+<summary>There is a broken Edge application in the Start menu. Is this normal?</summary>
+This is perfectly normal, UninstallAllEdgeChromium doesn't do a good job a lot of times.
+</details>
+<details>
+<summary>My ISO contains OneDrive. Why?</summary>
+OneDrive is not removed because depending on the user's threat model, it might be useful. You can uninstall it if you wish, though some ISOs won't contain it.
+</details>
+<details>
+<summary>Building failed!</summary>
+This is usually because you used a 2016- image, or an IoT image. You can fix this by renaming the index to <code>Windows 10 Enterprise LTSC</code>, but it will be untested, and as such, no support. It is recommended to use the latest stable build provided in the releases page.
+</details>
+<details>
+<summary>My checksums don't match!</summary>
+If you haven't synced the checksum file along with the ISO, the checksums won't match since every ISO created will be unique. This is a technical limitation of the tools that the build script uses. Otherwise, the ISO may have been tampered with or corrupted.
+</details>
+<!--
+<details>
+<summary></summary>
+</details>
+-->
---
+++ build.ps1
@@ -0,0 +1,109 @@
+Set-Location (Split-Path -Parent $MyInvocation.MyCommand.Path)
+
+if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
+    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
+        $CommandLine = "-File `"" + $MyInvocation.MyCommand.Path + "`" " + $MyInvocation.UnboundArguments
+        Start-Process -FilePath PowerShell.exe -Verb Runas -ArgumentList $CommandLine
+        Exit
+    }
+}
+
+if (!(Test-Connection github.com -Quiet -Count 2)) {
+    Write-Host "Please connect to the Internet."
+    Pause
+    Exit
+}
+
+if (!(Test-Path "DVD.iso")) {
+    Write-Host "Copy an LTSC 2021+ ISO to 'DVD.iso'. 32-bit and IoT are not recommended and untested."
+    Pause
+    Exit
+}
+
+$currentDate = Get-Date -Format "yy-MM-dd"
+
+if (Test-Path "DVD") {
+    Write-Host "Deleting DVD folder"
+    Remove-Item DVD -Recurse -Force
+}
+
+Write-Host "Extracting ISO"
+
+.\7z.exe x DVD.iso -oDVD
+
+Write-Host "Downloading Firefox and MAS"
+
+Start-BitsTransfer -Source "https://download.mozilla.org/?product=firefox-msi-latest-ssl&os=win&lang=en-US" -Destination "`$OEM$\`$$\Setup\Scripts\Firefox.msi" -Description "Downloading Firefox"
+Start-BitsTransfer -Source "https://raw.githubusercontent.com/massgravel/Microsoft-Activation-Scripts/master/MAS/All-In-One-Version/MAS_AIO.cmd" -Destination "`$OEM$\`$$\Setup\Scripts\MAS_AIO.cmd" -Description "Downloading MAS"
+
+if (Test-Path .\DVD\sources\install.esd) {
+    Write-Host "Converting ESD to WIM"
+    Export-WindowsImage -SourceImagePath .\DVD\sources\install.esd -SourceName "Windows 10 Enterprise LTSC" -DestinationImagePath .\DVD\sources\install.wim -CompressionType fast
+    Remove-Item .\DVD\sources\install.esd -Force
+} 
+
+Write-Host "Deleting N version"
+
+Remove-WindowsImage -ImagePath .\DVD\sources\install.wim -Name "Windows 10 Enterprise N LTSC"
+
+Write-Host "Mounting image"
+
+mkdir mount
+Mount-WindowsImage -ImagePath .\DVD\sources\install.wim -Index 1 -Path mount
+
+Write-Host "Loading hives"
+
+reg load HKEY_USERS\ODU mount\users\default\ntuser.dat
+reg load HKEY_LOCAL_MACHINE\SOFT mount\windows\system32\config\software
+
+Write-Host "Applying tweaks"
+
+reg import tweaks.reg
+
+Write-Host "Adding Wallpaper"
+
+takeown /f .\mount\Windows\Web /r /d y | Out-Null
+icacls .\mount\Windows\Web /grant administrators:f /t | Out-Null
+Get-ChildItem -Path .\mount\Windows\Web -Filter "*.jpg" -Recurse | ForEach-Object { Copy-Item .\img0.jpg $_.FullName -Force }
+
+Write-Host "Adding RunOnce"
+
+$startupFolder = "mount\Users\Default\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+
+if (!(Test-Path $startupFolder)) {
+    mkdir $startupFolder
+}
+
+Copy-Item .\RunOnce.cmd $startupFolder
+
+Write-Host Unloading hives
+
+reg unload HKEY_USERS\ODU
+reg unload HKEY_LOCAL_MACHINE\SOFT
+
+Write-Host Unmounting image
+
+Dismount-WindowsImage -Path mount -Save
+
+Remove-Item mount -Force
+
+Write-Host Copying OEM folder
+
+robocopy "`$OEM$" "DVD\sources\`$OEM$" /e
+
+Write-Host Copying autounattend files
+
+Copy-Item autounattend.xml DVD -Force
+
+Write-Host Creating ISO
+
+$isoName = "CamoOS_built_$currentDate.iso"
+
+cmd.exe /c "oscdimg.exe -h -m -o -u2 -udfver102 -bootdata:2#p0,e,bDVD\boot\etfsboot.com#pEF,e,bDVD\efi\microsoft\boot\efisys.bin -lCamoOS DVD $isoName"
+
+if (Test-Path $isoName) {
+    $hash = (Get-FileHash $isoName -Algorithm SHA1).Hash.ToLower()
+    Write-Host "Checksum is $hash (SHA1)."
+    Set-Content -Path ($isoName + ".sha1") -Value $hash -Encoding Ascii
+    Write-Host "Saved checksum."
+}
