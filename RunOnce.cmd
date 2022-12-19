c7f55a9a10108d2774e76af2b08168fc5b123364
0xC4aE1e5 <111705094+0xC4aE1e5@users.noreply.github.com>
2022-12-19 15:31:18 -0700

    12-19-2022 Rollup

--- RunOnce.cmd
+++ RunOnce.cmd
@@ -2,5 +2,7 @@
 
 :: Reboot
 
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "SystemUsesLightTheme" /t REG_DWORD /d "0" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v "AppsUseLightTheme" /t REG_DWORD /d "0" /f
 shutdown -r -t 5 -c "Required Reboot"
 del %0 /f
