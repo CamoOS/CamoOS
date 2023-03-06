4731f04085e00673d91a3834054025d7869518b0
0xC4aE1e5 <111705094+0xC4aE1e5@users.noreply.github.com>
2023-03-05 17:50:47 -0700

    Copy rundll32 rather than delete ShutUp10 when disabling

--- build.ps1
+++ build.ps1
@@ -102,8 +102,8 @@ robocopy "`$OEM$" "DVD\sources\`$OEM$" /e
 
 if ($env:CI -eq "true") {
     # Slim11 has a broken .NET
-    Write-Host Disabling ShutUp10
-    Remove-Item "DVD\sources\`$OEM$\`$$\Setup\Scripts\OOSU10.exe" -Force
+    Write-Host "Disabling ShutUp10"
+    Copy-Item "$env:systemroot\system32\rundll32.exe" "DVD\sources\`$OEM$\`$$\Setup\Scripts\OOSU10.exe" -Force
 }
 
 Write-Host Copying autounattend files
